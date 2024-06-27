a996f4423d93b2c96369765c79eb068794fac1da
chaehyeongyu <127322873+chaehyeongyu@users.noreply.github.com>
2024-06-27 23:17:47 +0900

    Add files via upload

---
+++ P0003_Longest_Substring_Without_Repeating_Characteres/P0003_Test.py
@@ -0,0 +1,9 @@
+from P0003_Longest_Substring_Without_Repeating_Characteres import Solution
+
+solution = Solution()
+sol = solution.lengthOfLongestSubstring('abcabcbb')
+print(sol)
+sol = solution.lengthOfLongestSubstring('bbbbb')
+print(sol)
+sol = solution.lengthOfLongestSubstring('pwwkew')
+print(sol)
